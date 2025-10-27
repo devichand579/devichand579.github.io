4db89b72f084dcaacbcf67468763861e599a3185
devichand579 <devichand579@gmail.com>
2025-10-27 09:22:37 +0000

    Deploying to gh-pages from @ devichand579/devichand579.github.io@40d00e24188c0aff1697c4b04297f85640f45049 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "Redirecting to CV...",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
