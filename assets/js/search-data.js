ddd7b0c14524c04dd52e0c8f2d88aaa4ef8fd557
devichand579 <devichand579@gmail.com>
2025-10-27 12:05:26 +0000

    Deploying to gh-pages from @ devichand579/devichand579.github.io@ff40b555b778639de14a08dc4c88f9bd7f6ef809 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "GitHub repositories and projects",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
         },{id: "nav-news",
           title: "news",
           description: "Latest news and announcements",
