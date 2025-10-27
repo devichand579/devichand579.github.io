cb09ffbb709e8e939904475d9b0f9654a97bd262
devichand579 <devichand579@gmail.com>
2025-10-27 19:44:46 +0000

    Deploying to gh-pages from @ devichand579/devichand579.github.io@789de4b229a2e4dabd500af63da7846b5448800c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,36 +58,14 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-      
-        title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-        
-      },
-    },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-      
-        title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-      
-      description: "",
-      section: "Posts",
-      handler: () => {
-        
-          window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-        
-      },
-    },{id: "news-started-my-undergraduate-journey-at-iit-kharagpur-at-computer-science-and-engineering-department-in-2021",
-          title: 'Started my undergraduate journey at IIT Kharagpur at Computer Science and Engineering department...',
+        },{id: "news-started-my-undergraduate-journey-at-iit-kharagpur-at-computer-science-and-engineering-department-in-2021",
+          title: 'Started my undergraduate journey at IIT Kharagpur at Computer Science and Engineering Department...',
           description: "",
           section: "News",},{id: "news-started-my-research-internship-at-saint-petersburg-electro-technical-university-russia-under-the-supervision-of-dr-dimitrii-kaplun",
           title: 'Started my research internship at Saint Petersburg Electro-technical University, Russia under the supervision...',
           description: "",
           section: "News",},{id: "news-started-my-research-internship-at-iit-palakkad-under-the-supervision-of-dr-sabarimalai-manikandan",
-          title: 'Started my research internship at IIT palakkad under the supervision of Dr.Sabarimalai manikandan....',
+          title: 'Started my research internship at IIT palakkad under the supervision of Dr.Sabarimalai Manikandan....',
           description: "",
           section: "News",},{id: "news-started-my-research-internship-at-queen-mary-university-of-london-under-the-supervision-of-dr-ahmed-m-a-sayed",
           title: 'Started my research internship at Queen Mary University of London under the supervision...',
